24cd3a908656755dbdeb6558974e97f1efc6c9d2
Ben <dadziebenjamin995@gmail.com>
2026-05-09 07:46:17 +0100

    Finished Hero section

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -7,6 +7,21 @@ export default function Home() {
       <a href="/">Journal</a>
       <a href="/">Contact</a>
       </div>
+      <div className="hero">
+      <h1>Hi Ben Here. 👋</h1>
+      <div className="hero-intro">
+      <p>21 year old computer science student at the University of Huddersfield</p>
+      <p>Full-stack developer in progress.</p>
+      </div>
+      <div className="hero-body">
+      <p>Building projects, learning by shipping and turning ideas into real products.</p>
+      <p>Currently focused on full-stack developments, learning software engineering principles and building real-world project</p>
+      </div>
+      <div className="hero-footer">
+      <b>For Q&A, start a chat with Ben Support</b>
+      <p>For any further contact, please refer to my social media platforms below.</p>
+      </div>
+      </div>
     </main>
   );
 }
